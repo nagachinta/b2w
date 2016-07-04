6230947a6f81702bded7ee632b1f56099008a93f
naga <cdev.me@gmail.com>
2016-07-04 13:11:57 -0700

    scripts added in index.html

--- express-server.js
+++ express-server.js
@@ -5,7 +5,7 @@ var port = process.env.PORT || 3000;
 
 // var pagePath = path.join(__dirname, 'pages');
 // var libPath = path.join(__dirname, 'lib');
-// var scriptPath = path.join(__dirname, 'script');
+// var scriptPath = path.join(__dirname, 'assets/js');
 
 app.use(express.static(__dirname));
 // app.use(express.static(pagePath));
@@ -14,5 +14,7 @@ app.use(express.static(__dirname));
 
 app.listen(port, function() {
     console.log('express HTTP Server Started: ', port);
-    console.log('Serving Files from: ', __dirname);   
+    console.log('Serving Files from: ', __dirname);
+    // console.log('Serving script files from: ', scriptPath);   
+
 });
