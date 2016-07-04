15526c72576c77884a03c8807cd165e842b14ae4
naga <cdev.me@gmail.com>
2016-07-04 12:20:52 -0700

    gitignore, modified server, index

---
+++ express-server.js
@@ -0,0 +1,18 @@
+var path = require('path');
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+
+// var pagePath = path.join(__dirname, 'pages');
+// var libPath = path.join(__dirname, 'lib');
+// var scriptPath = path.join(__dirname, 'script');
+
+app.use(express.static(__dirname));
+// app.use(express.static(pagePath));
+// app.use(express.static(libPath));
+// app.use(express.static(scriptPath));
+
+app.listen(port, function() {
+    console.log('express HTTP Server Started: ', port);
+    console.log('Serving Files from: ', __dirname);   
+});
